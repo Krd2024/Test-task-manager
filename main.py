23a1fa2820c0497418576c9ad79ffa7d5986af80
Krd2024 <polzov.ya@ya.ru>
2024-11-28 23:37:36 +0300

    Описание

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
 def write_read_file(tasks: list = None, choices: str = "w") -> None:
+    """
+    Функция для записи или чтения данных о книгах в/из файла "tasks.csv".
+
+    Аргументы:
+        tasks (list, optional): Список задач.
+        choices (str, optional): Режим работы с файлом.
+
+    - Если 'choices' равно "w", функция записывает
+        информацию о задачах  в файл "tasks.csv"
+        (id, name, description, category, period_execution,priority,status).
+
+    - Если 'choices' отличается от "w", функция читает строки из файла,
+        обрабатывает их и добавляет задачи в объект.
+
+    Исключения:
+        - При чтении файла: если строка в файле не соответствует
+          ожидаемому формату, выводится сообщение "Нет записи".
+
+    """
     task = TaskManager()
 
     with open("tasks.csv", choices, encoding="utf-8") as file:
@@ -6,7 +25,7 @@ def write_read_file(tasks: list = None, choices: str = "w") -> None:
             for task in tasks:
                 file.write(
                     f"{task.id},{task.name},{task.description},"
-                    f"{task.category},{task.period_execution}"
+                    f"{task.category},{task.period_execution},"
                     f"{task.priority},{task.status}\n"
                 )
         else:
@@ -26,6 +45,13 @@ def write_read_file(tasks: list = None, choices: str = "w") -> None:
 
 
 class Task:
+    """
+    Класс для представления задачи.
+
+    Счетчик уникальных идентификаторов (count_id)
+    увеличивается автоматически при создании каждой новой задачи.
+    """
+
     count_id = 1
 
     def __init__(
@@ -47,18 +73,20 @@ class Task:
         Task.count_id += 1
 
     def __str__(self):
+        """Строковое представление задачи"""
+
         return (
-            f"{self.name.upper()}.\n"
-            f"Категория: {self.category}\n\n"
-            f"Описание:\n{self.description}\n"
-            f"Срок выполнения: {self.period_execution}\n\n"
+            f"----- {self.name.upper()} -----\n"
+            f"Категория: {self.category}\n"
+            f"Описание: {self.description}\n"
+            f"Срок выполнения: {self.period_execution}\n"
             f"Приоритет: {self.priority}\n"
             f"Статус: {self.status}\n"
         )
 
 
 class TaskManager:
-    list_tasks = []  # Список задач
+    list_tasks = []  # Список всех задач
 
     def add_task(
         self,
@@ -68,19 +96,50 @@ class TaskManager:
         period_execution: int,
         priority: str,
     ):
+        """
+        Создаёт новую задачу и добавляет её в список.
+
+        Задача добавляется в список 'list_tasks' и записывается в файл.
+        """
+        # Создаёт оъект задачи
         task = Task(name, description, category, period_execution, priority)
+        # Заносит объек задачи в список задач
         self.list_tasks.append(task)
+        # Вызывает функцию записи в файл
+        # Передаёт список задач и флаг для "w" для записи
         write_read_file(self.list_tasks, choices="w")
 
-    def display_output(self) -> None:
+    def display_all_task(self) -> None:
         """
         Перебирает все задачи, и выводит информацию
         о каждой задаче. Если список пуст, выводится сообщение о том, что задачи отсутствуют.
         """
-        if not self.tasks:
+        if not self.list_tasks:
             print("Нет задач")
             return
-        print("Список задач:")
-        print("***")
-        for task in self.tasks:
+
+        print("\n СПИСОК ВСЕХ ЗАДАЧ:")
+        print(" " * 6)
+        for task in self.list_tasks:
             print(task)
+
+    def display_tasks_in_category(self, category):
+        """
+        Перебирает все задачи, и выводит информацию
+        о каждой задаче определенной категории. Если в данной категории нет задач,
+        выводится сообщение о том, что задачи в категории отсутствуют.
+        """
+        if not self.list_tasks:
+            print("\nСПИСОК ЗАДАЧ ПУСТ.\n")
+            return
+
+        tasks = [
+            task
+            for task in self.list_tasks
+            if task.category.lower() == category.lower()
+        ]
+        if len(tasks) == 0:
+            print(f"\nВ КАТЕГОРИИ {category.upper()} НЕТ ЗАДАЧ.\n")
+            return
+        print(f"\nСПИСОК ВСЕХ ЗАДАЧ В КАТЕГОРИИ - {category.upper()}:\n")
+        print(*tasks, sep="\n")
--- test_start.py
+++ test_start.py
@@ -1,7 +1,29 @@
 from main import TaskManager
 
 
-task_object = TaskManager()
-task_test = ("Дело", "Очень важное", "Работа", 3, "Высокий")
-for i in range(5):
-    task_object.add_task("Дело", "Очень важное", "Работа", 3, "Высокий")
+task_manager = TaskManager()
+# task_test = ("Дело", "Очень важное", "Работа", 3, "Высокий")
+# for i in range(5):
+#     task_manager.add_task(
+#         "Дело",
+#         "Описание, важного, дела",
+#         "Работа",
+#         "3 дня",
+#         "Высокий",
+#     )
+task_manager.add_task(
+    "Дело",
+    "Описание, важного, дела",
+    "Работа",
+    "3 дня",
+    "Высокий",
+)
+task_manager.add_task(
+    "Дело",
+    "Описание, важного, дела",
+    "Работа",
+    "3 дня",
+    "Высокий",
+)
+task_manager.display_all_task()
+# task_manager.display_tasks_in_category(category="РабоТА")
